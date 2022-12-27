5f02275a336f793cb632117dd6ffe7cca6af32b3
ygo <ka.ra.si.naporitan@gmail.com>
2022-12-27 15:06:01 +0900

    Content-Disposition追加

--- app/message_type/line_type/line_message.py
+++ app/message_type/line_type/line_message.py
@@ -140,7 +140,8 @@ class LineBotAPI:
             "https://upload.gyazo.com/api/upload",
             headers={
                 'Authorization': 'Bearer ' + os.environ['GYAZO_TOKEN'],
-                'Content-Type': 'multipart/form-data'
+                'Content-Type': 'multipart/form-data',
+                'Content-Disposition': 'attachment; filename=data.jpg'
             },
             files={
                 'imagedata': image_bytes
