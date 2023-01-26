c25a87925538a8d6771034865f52e0db77c3cc9d
ygo <ka.ra.si.naporitan@gmail.com>
2023-01-26 10:45:42 +0900

    代入先の変更

--- app/cogs/mst_line.py
+++ app/cogs/mst_line.py
@@ -59,26 +59,26 @@ class mst_line(commands.Cog):
 
         # ユーザーネームの空白文字を削除
         # message.author.name = message.author.name.replace("[\u3000 \t]", "", regex=True)
-        message.author.name = re.sub("[\u3000 \t]", "",message.author.name)
+        user_name = re.sub("[\u3000 \t]", "",message.author.name)
 
 
         # テキストメッセージ
-        messagetext=f"{message.channel.name}にて、{message.author.name}"
+        messagetext=f"{message.channel.name}にて、{user_name}"
 
         if message.type == discord.MessageType.new_member:
-            messagetext=f"{message.author.name}が参加しました。"
+            messagetext=f"{user_name}が参加しました。"
 
         if message.type == discord.MessageType.premium_guild_subscription:
-            messagetext=f"{message.author.name}がサーバーブーストしました。"
+            messagetext=f"{user_name}がサーバーブーストしました。"
 
         if message.type == discord.MessageType.premium_guild_tier_1:
-            messagetext=f"{message.author.name}がサーバーブーストし、レベル1になりました！！！！！！！！"
+            messagetext=f"{user_name}がサーバーブーストし、レベル1になりました！！！！！！！！"
 
         if message.type == discord.MessageType.premium_guild_tier_2:
-            messagetext=f"{message.author.name}がサーバーブーストし、レベル2になりました！！！！！！！！！"
+            messagetext=f"{user_name}がサーバーブーストし、レベル2になりました！！！！！！！！！"
 
         if message.type == discord.MessageType.premium_guild_tier_3:
-            messagetext=f"{message.author.name}がサーバーブーストし、レベル3になりました！！！！！！！！！！！"
+            messagetext=f"{user_name}がサーバーブーストし、レベル3になりました！！！！！！！！！！！"
 
         # 送付ファイルがあった場合
         if message.attachments:
