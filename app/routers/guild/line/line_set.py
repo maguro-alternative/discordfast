38d0b78e042dbbae383c7b14ca5b40678bf05e75
ygo <ka.ra.si.naporitan@gmail.com>
2023-05-29 17:29:56 +0900

    デバッグモードがオンのときcheckboxにチェックが付くよう修正

--- app/routers/guild/line/line_set.py
+++ app/routers/guild/line/line_set.py
@@ -153,13 +153,15 @@ async def line_set(
             line_bot_secret:str = await decrypt_password(encrypted_password=bytes(table.get('line_bot_secret')))
             line_group_id:str = await decrypt_password(encrypted_password=bytes(table.get('line_group_id')))
             default_channel_id:int = table.get('default_channel_id')
+            debug_mode:bool = bool(table.get('debug_mode',default=False))
 
             line_row = {
                 'line_notify_token':line_notify_token,
                 'line_bot_token':line_bot_token,
                 'line_bot_secret':line_bot_secret,
                 'line_group_id':line_group_id,
-                'default_channel_id':default_channel_id
+                'default_channel_id':default_channel_id,
+                'debug_mode':debug_mode
             }
 
 
