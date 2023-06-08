54b11f3fa713620a5a50a183af0058db76d54958
ygo <ka.ra.si.naporitan@gmail.com>
2023-06-08 18:02:07 +0900

    微調整

--- app/routers/login.py
+++ app/routers/login.py
@@ -124,8 +124,7 @@ async def line_login(
         request.session.pop("line_user")
     if request.session.get("line_oauth_data") != None:
         request.session.pop("line_oauth_data")
-    if request.session.pop('guild_id') != None:
-        request.session.pop('guild_id')
+    
 
     # ランダムなstate値の生成
     state = secrets.token_urlsafe(16)
