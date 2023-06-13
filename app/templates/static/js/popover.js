438eb6fdeb613a9206b2614baf1e5c3c65bb6eda
ygo <ka.ra.si.naporitan@gmail.com>
2023-06-13 12:43:32 +0900

    popoverに変更

---
+++ app/templates/static/js/popover.js
@@ -0,0 +1,15 @@
+const popoverBtn = document.getElementById('popover-btn');
+const popoverContent = document.getElementById('popover-content');
+
+popoverBtn.addEventListener('click', function() {
+  //console.log(popoverContent.style.display)
+  if (popoverContent.style.display === 'block'){
+    popoverContent.style.display = 'none';
+  }else if (popoverContent.style.display === 'none' || popoverContent.style.display == ''){
+    popoverContent.style.display = 'block';
+  }
+});
+
+popoverBtn.addEventListener('mouseout', function() {
+  popoverContent.style.display = 'none';
+});
