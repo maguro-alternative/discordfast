caf36d552ad8d6cb25ada27054643735612ea95f
ygo <ka.ra.si.naporitan@gmail.com>
2023-09-18 17:35:13 +0900

    reactをsessionに保持

--- app/routers/callback.py
+++ app/routers/callback.py
@@ -179,7 +179,6 @@ class CallBack(commands.Cog):
             request.session['line_user'] = line_id_token
 
             if request.session.get('react'):
-                request.session.pop('react')
                 return RedirectResponse(url=f'{os.environ.get("REACT_URL")}/guilds')
             else:
                 # ホームページにリダイレクトする
