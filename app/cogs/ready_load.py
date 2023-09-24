1bf1145952d1a6a0c9ae08ea80787ab9a69f16fb
ygo <ka.ra.si.naporitan@gmail.com>
2023-09-24 20:29:06 +0900

    expose_headersにorigin追加

--- app/cogs/ready_load.py
+++ app/cogs/ready_load.py
@@ -94,6 +94,7 @@ class ReadyLoad(commands.Cog):
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
+            expose_headers=["origin"]
         )
 
         # session使用
